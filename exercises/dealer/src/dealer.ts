138a8d5a4c5ed6d8266558a72ed19d6b403f6f0c
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-10-28 18:00:07 +0100

    dealer exercise

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,61 @@ function shuffleArray(a: any[]) {
   }
 }
 
-// export class Dealer {
+enum Suit {
+  Clubs,
+  Diamonds,
+  Suits,
+  Hearts
+}
 
-// }
+enum CardNumber {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King
+}
+
+type Card = [Suit, CardNumber];
+class Dealer {
+  cards: Card[];
+  constructor() {
+    this.cards = [];
+    // TODO: find way to loop over enums that doesn't make you feel like you need a shower
+    Object.values(Suit)
+      //only keep the numbers
+      .filter(val => typeof val === 'number')
+      .forEach(suit => {
+        Object.values(CardNumber)
+          .filter(val => typeof val === 'number')
+          .forEach(cardNumber => {
+            this.cards.push([suit, cardNumber]);
+          });
+      });
+    shuffleArray(this.cards)
+  }
+
+  readCard(card: Card): string {
+    const [suit, number] = card;
+    return `${CardNumber[number]} of ${Suit[suit]}`;
+  }
+  getLength(): number {
+    return this.cards.length;
+  }
+  dealHand(num: number): Card[] {
+    if (num <= this.cards.length && num > 0) {
+      return this.cards.splice(0, num);
+    }
+    throw new Error('invalid number of cards requested');
+  }
+}
+
+export { Suit, CardNumber, Dealer };
