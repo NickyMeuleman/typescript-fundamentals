6ca74a0509287887b102e138566750d890e8f8c4
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-11-01 21:34:02 +0100

    typed stack exercise

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -6,6 +6,26 @@ interface IStack<T> {
   print(): void;
 }
 
-// export class Stack<T> {
-
-// }
+export class Stack<T> implements IStack<T> {
+  private stack: T[];
+  constructor() {
+    this.stack = [];
+  }
+  length() {
+    return this.stack.length;
+  }
+  print() {
+    console.log(this.stack);
+  }
+  push(itemOrArray: T | T[]): IStack<T> {
+    if (itemOrArray instanceof Array) {
+      this.stack = this.stack.concat(itemOrArray);
+    } else {
+      this.stack.push(itemOrArray);
+    }
+    return this;
+  }
+  pop() {
+    return this.stack.pop();
+  }
+}
